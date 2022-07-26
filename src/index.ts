706cc4940f31327d12df43dc66e2aa4d564ee74a
Borstr <borstr@wp.pl>
2022-07-26 15:57:05 +0200

    trying to get mongodb to work

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 "use strict";
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
+const port = process.env.SERVER_PORT;
+const DBPassword = process.env.MONGODB_PASSWORD;
 
-app.get('/', (req, res) => {
-    console.log("Apollo GraphQL Express server is ready");
-});
+console.log(`mongodb://nadrukirekdb_MagazynNadrek:${DBPassword}@mongodb.nadrukirekdb.nazwa.pl:4063/nadrukirekdb_MagazynNadrek`);
 
-app.listen({ port: 8000 }, () => {
-    console.log(`Server is running at http://localhost:8080`);
-});
+mongoose
+    .connect(
+        `mongodb://nadrukirekdb_MagazynNadrek:mdlTpILoQ>3kJk3sbDi94l@mongodb.nadrukirekdb.nazwa.pl:4063/nadrukirekdb_MagazynNadrek`, 
+        { useNewUrlParser: true, user: 'nadrukirekdb_MagazynNadrek', pass: '' }
+    )
+    .then(() => {
+        // app.get('/', (req, res) => {
+        //     res.send('index')
+        // });
+        
+        // app.listen({ port }, () => {
+        //     console.log(`Server is running at http://localhost:8080`);
+        // });
+        console.log('test')
+    })
